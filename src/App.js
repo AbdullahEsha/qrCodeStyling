63a88bcf9b7342e35ee3455ba375b5321af5193a
AbdullahIsha <shahariaresha@gmail.com>
2022-05-12 18:13:07 +0600

    image upload color change feature complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Col, Container, Row } from "react-bootstrap";
 import QRCodeStyling from "qr-code-styling";
 import img from "./qrCode.jpg";
 
+const color1 = "#21D2FF";
+const color2 = "#A433FF";
+const color3 = "#ffffff";
+
 const qrCode = new QRCodeStyling({
   width: 300,
   height: 300,
@@ -16,16 +20,16 @@ const qrCode = new QRCodeStyling({
       type: "linear", //radial,
       rotation: Math.PI / 4,
       colorStops: [
-        { offset: 0, color: "#21D2FF" },
-        { offset: 0.7, color: "#A433FF" },
+        { offset: 0, color: color1 },
+        { offset: 0.7, color: color2 },
       ],
     },
   },
   cornersSquareOptions: {
-    type: "dots",
+    type: "dot",
   },
   cornersDotOptions: {
-    type: "dots",
+    type: "dot",
   },
   imageOptions: {
     crossOrigin: "anonymous",
@@ -38,7 +42,15 @@ const App = () => {
   const [width, setWidth] = useState(300);
   const [height, setHeight] = useState(300);
   const [margin, setMargin] = useState(0);
+  const [image, setImage] = useState("");
   const [imgMargin, setImgMargin] = useState(0);
+  const [dotsType, setDotsType] = useState("dots");
+  const [dotsColorS, setDotsColorS] = useState(color1);
+  const [dotsColorE, setDotsColorE] = useState(color2);
+  const [cornersSquareType, setCornersSquareType] = useState("dot");
+  const [cornersDotType, setCornersDotType] = useState("dot");
+  const [backgroundColorS, setBackgroundColorS] = useState(color3);
+  const [backgroundColorE, setBackgroundColorE] = useState(color3);
   const [fileExt, setFileExt] = useState("png");
   const ref = useRef(null);
 
@@ -49,16 +61,55 @@ const App = () => {
   useEffect(() => {
     qrCode.update({
       data: url,
+      image: image,
       width: width,
       height: height,
       margin: margin,
-      imageOptions: [(margin: imgMargin)],
+      imageOptions: { margin: imgMargin },
+      dotsOptions: {
+        type: dotsType,
+        gradient: {
+          colorStops: [
+            { offset: 0, color: dotsColorS },
+            { offset: 0.7, color: dotsColorE },
+          ],
+        },
+      },
+      cornersSquareOptions: { type: cornersSquareType },
+      cornersDotOptions: { type: cornersDotType },
+      backgroundOptions: {
+        gradient: {
+          colorStops: [
+            { offset: 0, color: backgroundColorS },
+            { offset: 0.7, color: backgroundColorE },
+          ],
+        },
+      },
     });
-  }, [url, width, height, margin, imgMargin]);
+  }, [
+    url,
+    image,
+    width,
+    height,
+    margin,
+    imgMargin,
+    dotsType,
+    cornersSquareType,
+    cornersDotType,
+    dotsColorS,
+    dotsColorE,
+    backgroundColorS,
+    backgroundColorE,
+  ]);
 
   const onUrlChange = (event) => {
     event.preventDefault();
     setUrl(event.target.value);
+  };
+
+  const onImageChange = (event) => {
+    console.log("Image: ", img);
+    setImage(URL.createObjectURL(event.target.files[0]));
   };
 
   const onWidthChange = (event) => {
@@ -74,6 +125,46 @@ const App = () => {
   const onMarginChange = (event) => {
     event.preventDefault();
     setMargin(event.target.value);
+  };
+
+  const onImgMarginChange = (event) => {
+    event.preventDefault();
+    setImgMargin(event.target.value);
+  };
+
+  const onDotsTypeChange = (event) => {
+    event.preventDefault();
+    setDotsType(event.target.value);
+  };
+
+  const onDotsColorSChange = (event) => {
+    event.preventDefault();
+    setDotsColorS(event.target.value);
+  };
+
+  const onDotsColorEChange = (event) => {
+    event.preventDefault();
+    setDotsColorE(event.target.value);
+  };
+
+  const onCornersSquareTypeChange = (event) => {
+    event.preventDefault();
+    setCornersSquareType(event.target.value);
+  };
+
+  const onCornersDotTypeChange = (event) => {
+    event.preventDefault();
+    setCornersDotType(event.target.value);
+  };
+
+  const onBackgroundColorSChange = (event) => {
+    event.preventDefault();
+    setBackgroundColorS(event.target.value);
+  };
+
+  const onBackgroundColorEChange = (event) => {
+    event.preventDefault();
+    setBackgroundColorE(event.target.value);
   };
 
   const onExtensionChange = (event) => {
@@ -112,7 +203,13 @@ const App = () => {
                   <h4>Image File</h4>
                 </Col>
                 <Col className="algnlf">
-                  <input type={"File"} className="form-control" />
+                  <input
+                    id="profilePic"
+                    type="file"
+                    onChange={onImageChange}
+                    //value={image}
+                    className="form-control"
+                  />
                 </Col>
               </Row>
               <br />
@@ -121,7 +218,14 @@ const App = () => {
                   <h4>Image Margin</h4>
                 </Col>
                 <Col className="algnlf ">
-                  <input type={"number"} className="form-control" min="0" />
+                  <input
+                    type={"number"}
+                    value={imgMargin}
+                    onChange={onImgMarginChange}
+                    className="form-control"
+                    min="0"
+                    max="30"
+                  />
                 </Col>
               </Row>
               <br />
@@ -180,12 +284,8 @@ const App = () => {
                   <div className="form-group">
                     <select
                       className="form-control"
-                      // name="n_person"
-                      // value={n_person}
-                      // onChange={(event) => {
-                      //   setNPerson(event.target.value);
-                      // }}
-                      required
+                      value={dotsType}
+                      onChange={onDotsTypeChange}
                     >
                       <option value="" disabled selected>
                         Choose One...
@@ -193,9 +293,9 @@ const App = () => {
                       <option value="square">Square</option>
                       <option value="dots">Dots</option>
                       <option value="rounded">Rounded</option>
-                      <option value="extraRounded">Extra Rounded</option>
+                      <option value="extra-rounded">Extra Rounded</option>
                       <option value="classy">Classy</option>
-                      <option value="classyRounded">Classy Rounded</option>
+                      <option value="classy-rounded">Classy Rounded</option>
                     </select>
                   </div>
                 </Col>
@@ -206,49 +306,49 @@ const App = () => {
                   <h4>Dots Color</h4>
                 </Col>
                 <Col className="algnlf">
-                  <input type={"color"} /> <input type={"color"} />
-                  {/* <input value="" onChange={} /> */}
+                  <input
+                    type={"color"}
+                    value={dotsColorS}
+                    onChange={onDotsColorSChange}
+                  />{" "}
+                  <input
+                    type={"color"}
+                    value={dotsColorE}
+                    onChange={onDotsColorEChange}
+                  />
                 </Col>
               </Row>
               <br />
               <Row>
-                <Col className="algn ">
+                <Col className="algn">
                   <h4>Corners Square Style</h4>
                 </Col>
                 <Col className="algnlf ">
                   <div className="form-group">
                     <select
                       className="form-control"
-                      // name="n_person"
-                      // value={n_person}
-                      // onChange={(event) => {
-                      //   setNPerson(event.target.value);
-                      // }}
-                      required
+                      value={cornersSquareType}
+                      onChange={onCornersSquareTypeChange}
                     >
                       <option value="" disabled selected>
                         Choose One...
                       </option>
                       <option value="square">Square</option>
-                      <option value="dots">Dots</option>
-                      <option value="rounded">Rounded</option>
-                      <option value="extraRounded">Extra Rounded</option>
-                      <option value="classy">Classy</option>
-                      <option value="classyRounded">Classy Rounded</option>
+                      <option value="dot">Dot</option>
+                      <option value="extra-rounded">Extra Rounded</option>
                     </select>
                   </div>
                 </Col>
               </Row>
-              <br />
-              <Row>
+              {/*<br />
+               <Row>
                 <Col className="algn">
                   <h4>Corners Square Color</h4>
                 </Col>
                 <Col className="algnlf">
                   <input type={"color"} /> <input type={"color"} />
-                  {/* <input value="" onChange={} /> */}
                 </Col>
-              </Row>
+              </Row> */}
               <br />
               <Row>
                 <Col className="algn ">
@@ -258,44 +358,43 @@ const App = () => {
                   <div className="form-group">
                     <select
                       className="form-control"
-                      // name="n_person"
-                      // value={n_person}
-                      // onChange={(event) => {
-                      //   setNPerson(event.target.value);
-                      // }}
-                      required
+                      value={cornersDotType}
+                      onChange={onCornersDotTypeChange}
                     >
                       <option value="" disabled selected>
                         Choose One...
                       </option>
                       <option value="square">Square</option>
-                      <option value="dots">Dots</option>
-                      <option value="rounded">Rounded</option>
-                      <option value="extraRounded">Extra Rounded</option>
-                      <option value="classy">Classy</option>
-                      <option value="classyRounded">Classy Rounded</option>
+                      <option value="dot">Dot</option>
                     </select>
                   </div>
                 </Col>
               </Row>
-              <br />
+              {/* <br />
               <Row>
                 <Col className="algn">
                   <h4>Corners Dot Color</h4>
                 </Col>
                 <Col className="algnlf">
                   <input type={"color"} /> <input type={"color"} />
-                  {/* <input value="" onChange={} /> */}
                 </Col>
-              </Row>
+              </Row> */}
               <br />
               <Row>
                 <Col className="algn">
                   <h4>Background Color</h4>
                 </Col>
                 <Col className="algnlf">
-                  <input type={"color"} /> <input type={"color"} />
-                  {/* <input value="" onChange={} /> */}
+                  <input
+                    type={"color"}
+                    value={backgroundColorS}
+                    onChange={onBackgroundColorSChange}
+                  />{" "}
+                  <input
+                    type={"color"}
+                    value={backgroundColorE}
+                    onChange={onBackgroundColorEChange}
+                  />
                 </Col>
               </Row>
               {/*<br />
@@ -315,19 +414,19 @@ const App = () => {
               </Row> */}
             </Col>
             <Col>
-              <div ref={ref} />
+              <div ref={ref} id="bdr" />
               <br />
               <br />
               <select
                 onChange={onExtensionChange}
                 value={fileExt}
-                class="btn btn-outline-primary"
+                class="btn btn-outline-primary btn-lg"
               >
                 <option value="png">PNG</option>
                 <option value="jpeg">JPEG</option>
                 <option value="webp">WEBP</option>
               </select>{" "}
-              <button onClick={onDownloadClick} class="btn btn-primary ">
+              <button onClick={onDownloadClick} class="btn btn-primary btn-md">
                 Download
               </button>
             </Col>
